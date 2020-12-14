06ed77b18140fc92955982104be43b6ded2cd7b1
kylesmith <kylelsmith47@gmail.com>
2020-12-13 22:51:18 -0700

    12/13

---
+++ For Fun/1. Data Types/Examples/Bools.py
@@ -0,0 +1,11 @@
+"""
+Bools or Booleans are simply true/false statements. You can think of it like a light switch. Either the light is on
+or the light is off.
+"""
+
+are_you_awesome = True
+do_i_owe_you_five_dollars = False
+
+"""
+Equally 1 is equivalent to True and 0 to False
+"""
---
+++ For Fun/1. Data Types/Examples/Numbers.py
@@ -0,0 +1,54 @@
+"""
+There are a few different ways that computers represent numbers.
+The two main ways that I'll talk about are
+Integers
+Floats
+
+Integers are numbers with no decimal points so, 1, 913414, -30, 0
+
+Floats are numbers with decimal points: 3.14159, .5, -3.9
+"""
+
+
+"""
+Integers
+
+You can modify these numbers as you would normally on a calculator
++: addition
+-: subtraction
+*: multiplication
+/: division
+**: powers (2**5 is 2^5)
+"""
+
+Age = 22
+Height = 31
+
+x = 15
+y = 30
+addition = x + y
+multiplication = x*y
+subtraction = x - y
+division = x/y
+
+"""
+Floats
+
+Floats have same methods (ways to modify it) as integers, or ints. Floats are basically numbers that have
+decimal numbers. ex) 1.0, 0.2, .00004, ect.)
+"""
+
+percentage = .75
+one_half = .5
+
+
+################
+#    EXTRAS    #
+################
+"""
+Other things to be away of:
+
+* modulus or mod, done by using %. Modulo is basically the remainder from long division.
+
+20%3 = 2 (3*6 = 18, 20-18 = 2)
+"""
