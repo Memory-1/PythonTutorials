0ef6d12e6eccb27393b2c19e35013ee22598c847
kylesmith <kylelsmith47@gmail.com>
2021-01-02 00:44:27 -0700

    Added a description

--- 2. Logic and Loops/Projects/Project 2 - Hangman.py
+++ 2. Logic and Loops/Projects/Project 2 - Hangman.py
@@ -1,3 +1,27 @@
+"""
+This project may be a bit more challenging. There is a lot which I haven't included. However, I do provide
+some to-do chunks to guide you on what you should do. These chunks are at the correct tab location. It's important
+that you keep it at the same tabbed location.
+
+Hangman:
+
+This game will choose a random word and the user will be prompted to guess a letter. If the letter is correct, then it
+will display it at the correct location in word.
+
+If the user guesses a letter wrong, they'll get a strike. On the sixth incorrect guess, the user loses.
+
+If the user has guessed all of the correct letters. break of out the loop (use the keyword break)
+
+I've provided a way to display the word with the correct letters and underscores for the missing letters
+ex) if the word was test, and the user has guessed t. generate_remaining_word() will return "T _ _ T"
+
+
+Your challenge is to practice if statements and do the necessary code in that as outlined with the to do statements.
+
+(I believe) I've included everything you need to complete this.
+"""
+
+
 import random
 
 wordbank = ["abruptly", "absurd", "abyss", "affix", "askew", "avenue", "awkward", "axiom", "azure", "bagpipes", "bandwagon", "banjo", "bayou", "beekeeper", "bikini", "blitz", "blizzard", "boggle", "bookworm", "boxcar", "boxful", "buckaroo", "buffalo", "buffoon", "buxom", "buzzard", "buzzing", "buzzwords", "caliph", "cobweb", "cockiness", "croquet", "crypt", "curacao", "cycle", "daiquiri", "dirndl", "disavow", "dizzying", "duplex", "dwarves", "embezzle", "equip", "espionage", "euouae", "exodus", "faking", "fishhook", "fixable", "fjord", "flapjack", "flopping", "fluffiness", "flyby", "foxglove", "frazzled", "frizzled", "fuchsia", "funny", "gabby", 'happy', 'pencil', "stronghold", "stronghold", "stymied", "subway", "swivel", "syndrome", "thriftless", "thumbscrew", "topaz", "transcript", "transgress", "transplant", "triphthong", "twelfth", "twelfths", "unknown", "unworthy", "unzip", "uptown", "vaporize", "vixen", "vodka", "voodoo", "vortex", "voyeurism", "walkway", "waltz", "wave", "wavy", "waxy", "wellspring", "wheezy", "whiskey", "whizzing", "whomever", "wimpy", "witchcraft", "wizard", "woozy", "wristwatch", "wyvern", "xylophone", "yachtsman", "yippee", "yoked", "youthful", "yummy", "zephyr", "zigzag"]
